4314c3ffd93c2d022cf8b072491d5d4ef5056872
the80hz <dnl.vlgv@mail.ru>
2022-09-20 17:04:38 +0400

    точно финал изменил библиотеку чтобы не было ошибки с переименованием полных папок

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import os
+import shutil
 from random_all import random_int
 
 
 if __name__ == '__main__':
     pathname = input('Введите путь: ')
+    n = 999
 
     # проходимся по всем папкам в директории
     for entry in os.listdir(pathname):
         if os.path.isdir(os.path.join(pathname, entry)):
-            value = random_int(1, 20)
+            value = random_int(1, n)
 
             # проверяем, не занято ли число
             for folder in os.listdir(pathname):
                 while folder == value:
-                    value = random_int(1, 20)
+                    value = random_int(1, n)
 
             # переименование
-            os.rename(pathname + '/' + entry, pathname + '/' + value)
+            shutil.move(pathname + '/' + entry, pathname + '/' + value)
