2c5d9f4381bc92f8ce1a267882006b802336ca68
the80hz <dnl.vlgv@mail.ru>
2022-09-20 16:53:04 +0400

    вроде финал

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
-from random import randint
+from random_all import random_int
 
 
 if __name__ == '__main__':
-    # pathname = input('Введите путь: ')
-    # pathname = '/Users/the80hz/Yandex.Disk.localized/poggers'
-    pathname = '/Users/the80hz/Downloads/'
+    pathname = input('Введите путь: ')
+
+    # проходимся по всем папкам в директории
     for entry in os.listdir(pathname):
         if os.path.isdir(os.path.join(pathname, entry)):
-            value = str(randint(1, 20))
+            value = random_int(1, 20)
+
+            # проверяем, не занято ли число
+            for folder in os.listdir(pathname):
+                while folder == value:
+                    value = random_int(1, 20)
+
+            # переименование
             os.rename(pathname + '/' + entry, pathname + '/' + value)
---
+++ random_all.py
@@ -0,0 +1,6 @@
+from random import randint
+
+
+def random_int(i, j):
+    value = str(randint(i, j))
+    return value
